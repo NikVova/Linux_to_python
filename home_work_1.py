1e4e453dc75a7ed6331a60c327947172186e892a
NikVova <isak25@mail.ru>
2023-04-23 09:35:49 +0300

    add task_1_home_work_1

--- home_work_1.py
+++ home_work_1.py
@@ -1,6 +1,6 @@
 import subprocess
-
-# task_3_seminar
+import re
+"task_3_seminar"
 # if __name__ == '__main__':
 #     result = subprocess.run('cat /etc/os-release\n', shell=True,
 #                             stdout=subprocess.PIPE, encoding='utf 8')
@@ -17,3 +17,23 @@ import subprocess
 #             print('FAIL')
 #     else:
 #         print('FAIL')
+
+
+"task_1_home_work"
+# if __name__ == '__main__':
+#
+#     def func(com: str, text: str):
+#         result = subprocess.run(com, shell=True,
+#                                 stdout=subprocess.PIPE, encoding='utf 8')
+#         out = result.stdout
+#         # print(out)
+#         if result.returncode == 0:
+#             if re.search(text, out):
+#                 print(True)
+#             else:
+#                 print(False)
+#         else:
+#             print(False)
+#
+#
+#     func('cat /etc/os-release', 'jammy')
