78c521582b9ebc288ec507ed0d4348511da84b47
NikVova <isak25@mail.ru>
2023-04-23 22:33:43 +0300

    add Home_work_2_task_1

--- Seminar_2/test_7z_pozitive.py
+++ Seminar_2/test_7z_pozitive.py
@@ -4,7 +4,6 @@ path_dir = "/home/user/tst"
 path_arx = "/home/user/arx1"
 
 
-
 def test_step1():
     assert checkout("cd {}; 7z a {}".format(path_dir, path_arx), "Everything is Ok"), "Test1 Fail"
 
@@ -12,5 +11,5 @@ def test_step1():
 def test_step2():
     assert checkout("cd {}; 7z u {}".format(path_dir, path_arx), "Everything is Ok"), "Test1 Fail"
 
-# def test_step3():
-#     assert checkout("cd {}; 7z d {}".format(path_dir, path_arx), "Everything is Ok"), "Test1 Fail"
+def test_step3():
+    assert checkout("cd {}; 7z d {}".format(path_dir, path_arx), "Everything is Ok"), "Test1 Fail"
