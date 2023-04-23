c8a0896729af79e1e5b48f34416529565edf184c
NikVova <isak25@mail.ru>
2023-04-23 09:42:53 +0300

    add task_2_home_work_1

--- home_work_1.py
+++ home_work_1.py
@@ -1,5 +1,7 @@
 import subprocess
 import re
+import string
+
 "task_3_seminar"
 # if __name__ == '__main__':
 #     result = subprocess.run('cat /etc/os-release\n', shell=True,
@@ -37,3 +39,24 @@ import re
 #
 #
 #     func('cat /etc/os-release', 'jammy')
+
+"task_2_home_work_1"
+
+# if __name__ == '__main__':
+#
+#     def func(com: str, text: str):
+#         result = subprocess.run(com, shell=True,
+#                                 stdout=subprocess.PIPE, encoding='utf 8')
+#         out = result.stdout
+#         for p in string.punctuation:
+#             if p in out:
+#                 out_rep = out.replace(p, ' ')
+#
+#         if result.returncode == 0:
+#             if re.search(text, out_rep):
+#                 print(True)
+#             else:
+#                 print(False)
+#         else:
+#             print(False)
+#     func('cat /etc/os-release', '22.04.1')
