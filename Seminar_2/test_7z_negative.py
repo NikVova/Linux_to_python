844e42694567fbd1f185867c4dc68242a6e1295c
NikVova <isak25@mail.ru>
2023-04-30 09:46:49 +0300

    rename_seminar_3

--- Seminar_2/test_7z_negative.py
+++ Seminar_2/test_7z_negative.py
@@ -1,10 +1,10 @@
-from Seminar_2.checkout import checkout
-
-path_dir = "/home/user/tst"
-path_arx = "/home/user/arx2.7z"
-path_to_dir = "/home/user/out"
-
-
+# from Seminar_2.checkout import checkout
+#
+# path_dir = "/home/user/tst"
+# path_arx = "/home/user/arx2.7z"
+# path_to_dir = "/home/user/out"
+#
+#
 # def test_step4():
 #     assert checkout("cd {}; 7z e {} -o{}".format(path_dir, path_arx, path_to_dir), "ERRORS:"), "Test1 Fail"
 #
