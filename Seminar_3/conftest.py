3ad2e6e1ac5631f3bcf6afc40c8693e4daaa6440
NikVova <isak25@mail.ru>
2023-05-01 09:09:33 +0300

    add new file

--- Seminar_3/conftest.py
+++ Seminar_3/conftest.py
@@ -2,31 +2,41 @@ import random
 import string
 import subprocess
 import pytest
+import yaml
+
 from checkout import checkout_positive
 
-folder_in = "/home/user/tst/file"
-folder_out = "/home/user/tst/out"
-folder_ext = "/home/user/tst/ext"
-folder_badarx = "/home/user/tst/badarx"
+# folder_in = "/home/user/tst/file"
+# folder_out = "/home/user/tst/out"
+# folder_ext = "/home/user/tst/ext"
+# folder_ext2 = "/home/user/tst/ext2"
+# folder_badarx = "/home/user/tst/badarx"
+
+with open("config.yaml") as f:
+    data = yaml.safe_load(f)
 
 
 @pytest.fixture()
 def make_folders():
-    return checkout_positive("mkdir {} {} {} {}".format(folder_in, folder_out, folder_ext, folder_badarx), "")
+    return checkout_positive(
+        "mkdir {} {} {} {} {}".format(data["folder_in"], data["folder_out"], data["folder_ext"], data["folder_ext2"],
+                                      data["folder_badarx"]), "")
 
 
 @pytest.fixture()
 def clear_folders():
-    return checkout_positive("rm -rf {}/* {}/* {}/* {}/*".format(folder_in, folder_out, folder_ext, folder_badarx), "")
+    return checkout_positive(
+        "rm -rf {}/* {}/* {}/* {}/* {}/*".format(data["folder_in"], data["folder_out"], data["folder_ext"],
+                                                 data["folder_ext2"], data["folder_badarx"]), "")
 
 
 @pytest.fixture()
 def make_files():
     list_off_files = []
-    for i in range(5):
+    for i in range(data["count"]):
         filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
         if checkout_positive(
-                "cd {}; dd if=/dev/urandom of={} bs=1M count=1 iflag=fullblock".format(folder_in, filename),
+                "cd {}; dd if=/dev/urandom of={} bs=1M count=1 iflag=fullblock".format(data["folder_in"], filename),
                 ""):
             list_off_files.append(filename)
     return list_off_files
@@ -36,11 +46,18 @@ def make_files():
 def make_subfolder():
     testfilename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     subfoldername = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-    if not checkout_positive("cd {}; mkdir {}".format(folder_in, subfoldername), ""):
+    if not checkout_positive("cd {}; mkdir {}".format(data["folder_in"], subfoldername), ""):
         return None, None
     if not checkout_positive(
-            "cd {}/{}; dd if=/dev/urandom of={} bs=1M count=1 iflag=fullblock".format(folder_in, subfoldername,
+            "cd {}/{}; dd if=/dev/urandom of={} bs=1M count=1 iflag=fullblock".format(data["folder_in"], subfoldername,
                                                                                       testfilename), ""):
         return subfoldername, None
     else:
         return subfoldername, testfilename
+
+
+@pytest.fixture()
+def make_bad_arx(make_folders, clear_folders, make_files):
+    checkout_positive("cd {}; 7z a {}/badarx.7z".format(data["folder_in"], data["folder_badarx"]),
+                      "Everything is Ok"), "Test1 Fail"
+    return checkout_positive("truncate -s 1 {}/badarx.7z".format(data["folder_badarx"]), ""), "Test1 Fail"
